ad63c13733c8a1ff96b08b0e6e092f367a6777e2
KarlieFang <61367497+KarlieFang@users.noreply.github.com>
2020-04-06 19:03:32 -0500

    Update word2vec.py

--- word2vec.py
+++ word2vec.py
@@ -1,8 +1,9 @@
-from gensim.models import word2vec
+from gensim.models import word2vec #word2vec is a package we can use
 import logging
 
-PATH2SOGOUT = ''
+PATH2SOGOUT = '' #file path
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-sentences = word2vec.LineSentence(PATH2SOGOUT)
-model = word2vec.Word2Vec(sentences, workers=20, sg=1, size=300, min_count=50)
-model.wv.save_word2vec_format('300Tvectors.txt', binary=False)
+sentences = word2vec.LineSentence(PATH2SOGOUT) #one line is one sentence
+model = word2vec.Word2Vec(sentences, workers=20, sg=1, size=300, min_count=50)  #Train, use and evaluate neural networks 
+model.wv.save_word2vec_format('300Tvectors.txt', binary=False) # Store the model with txt format(?)
+#The trained word vectors can also be stored/loaded from a format compatible with the original word2vec implementation
